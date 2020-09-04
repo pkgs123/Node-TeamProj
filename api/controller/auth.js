58f638af90869834acb4dd4d66e0850e4e20e670
Prashant Singh <prashant.55555@yahoo.co.in>
2020-09-04 12:46:54 +0530

    Signout logic changes

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -13,9 +13,10 @@ exports.signup  = (req , res)=>{
     }
     const user = new User(req.body)
     user.save((err ,user)=>{
+        console.log(err);
         if(err){
             return res.status(400).json({
-                err : "Unable to singup"
+                error : `${err.keyValue.email} already present`
             });
         } 
 
